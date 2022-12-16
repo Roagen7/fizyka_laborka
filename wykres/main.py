2c388e671f82e16540d3be4e8b3b86411f6c6d9b
Dominik Lau <harek2388@gmail.com>
2022-12-16 23:52:04 +0100

    e5 1

--- wykres/main.py
+++ wykres/main.py
@@ -22,21 +22,22 @@ def stdev(a, x, y): #stdev = S_a
 if __name__ == '__main__':
    # matplotlib.use('Windows')
 
-    error = [0.00, 0.00, 0.01, 0.01, 0.01, 0.01, 0.01, 0.02, 0.02, 0.02] #zmienić todo
-    x = [0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]#zmienić todo
-    y = [3.24, 7.16, 10.80, 14.62, 18.26, 21.99, 26.50, 29.74, 33.96, 37.59]#zmienić todo
+    error = [0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.02, 0.02, 0.02]
+    error_x = [0.02 for x in range(len(error))]
+    x = [0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]
+    y = [3.24, 7.16, 10.80, 14.62, 18.26, 21.99, 26.50, 29.74, 33.96, 37.59]
 
     coeff = np.polyfit(x,y,1)
     dev = stdev(coeff[0], x, y)
 
     plot(x,y, 'yo', x, np.poly1d(coeff)(x),  c='0.88',markersize=0.01)
-    _, caps, _ = errorbar(x, y, error, fmt='o',  markersize=0.01, capsize=1)
+    _, caps, _ = errorbar(x, y, yerr = error,xerr = error_x, fmt='o',  markersize=0.01, capsize=1)
 
     for cap in caps:
         cap.set_markeredgewidth(1)
 
-    plt.ylabel('F [N]') #zmienić todo
-    plt.xlabel('I [V]') #zmienić todo
+    plt.ylabel('F [mN]')
+    plt.xlabel('I [A]')
 
     print("a=", coeff)
     print("stdev=", dev)
